12e15d2e1fab16c383bb341660149a80d3f9b86b
Glenn Vlekke <g.vlekke@sping.nl>
2017-08-08 21:15:06 +0200

    add listen to port to the express server

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
-
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
 
 client.on('ready', () => {
   console.log('Blanche: I am ready!');
