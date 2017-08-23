dd1586b64a5df4835962a872799d8c6ecc704020
petiebarie <30798422+petiebarie@users.noreply.github.com>
2017-08-23 17:00:35 +0200

    Minor text fixes: i made things look a little prettier, changed some text and removed the !type and ![teamname] since i can't get them to work

--- index.js
+++ index.js
@@ -1,251 +1,208 @@
-'use strict';
+"use strict";
 
 
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-require('dotenv').config()
+require("dotenv").config()
 
-var Discord = require('discord.js');
+var Discord = require("discord.js");
 var client = new Discord.Client();
 var https = require("https");
 const Message = require("./models/message")
 const settings = require("./settings.json")
-const gyms = require('./data/gyms.json');
-const stops = require('./data/pokestops.json');
-const pokemons = require('./data/pokemons.json');
-const defense = require('./data/defense.json');
-const messages = require('./data/messages.json');
+const gyms = require("./data/gyms.json");
+const stops = require("./data/pokestops.json");
+const pokemons = require("./data/pokemons.json");
+const defense = require("./data/defense.json");
+const messages = require("./data/messages.json");
 
 client.login(process.env.TOKEN);
 
 
-var express = require('express');
+var express = require("express");
 var app = express();
 
-const Gym = require('./helpers/gym.js');
+const Gym = require("./helpers/gym.js");
 
-const pokemonStats = require('./helpers/pokemon.js');
+const pokemonStats = require("./helpers/pokemon.js");
 pokemonStats.calculateRanks();
 
-const raids = require('./helpers/raids.js');
+const raids = require("./helpers/raids.js");
 raids.init();
 
-app.set('port', (process.env.PORT || 9222));
-
-app.use(express.static(__dirname + '/public'));
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-
-  setInterval(function() {
-    https.get("https://discord-pokemon.herokuapp.com/helloworld.html");
-  }, 300000); // every 5 minutes (300000)
-});
-
-client.on('ready', () => {
-  client.user.setGame('Pokemon Go');
-  console.log('Blanche: I am ready!');
-});
-
 client.on("message", async (msg) => {
-  //console.log(msg)
-  let prefixs = settings.prefixs;
+let prefixs = settings.prefixs;
+let moderator = settings.moderator;
 
-  let moderator = settings.moderator;
+// get the first character of the message content, remove it and convert the rest to lowercase
+let msgPrefix = msg.content[0];
+var msgText = msg.content.toLowerCase().substr(1);
+var pokemon, gym;
 
-  // get the first character of the message content
-  let msgPrefix = msg.content[0];
-  // get the message content, cut the first character, convert it to all lowercase and save to new variable
-  var msgText = msg.content.toLowerCase().substr(1);
-  var pokemon, gym;
+//determine if the bot activates or not
+if (prefixs.indexOf(msgPrefix) < 0 || msg.author.bot) return;
 
-  if (prefixs.indexOf(msgPrefix) < 0 || msg.author.bot) return;
-
-  if (msgText.split(' ')[0] == 'raid') {
-
-    raids.scan(msg);
-  }
+//raid reply
+if (msgText.split(' ')[0] == "raid") {
+    raids.scan(msg);}
 
 //text replies with extra options
 else if (msgText === "dobbel" || msgText === "gamble") {
- msg.reply("<:game_die:349868481673428992>: " + (Math.floor(Math.random()*6)+1));
- msg.delete()}
-else if (msgText === "type") {message.channel.send(
-  "Zie hier een tabel met type effectiviteit:", {file: "https://image.ibb.co/mxRthv/Untitled.png"});
-  msg.delete()}
-else if (msgText === "mystic") {message.channel.send(
- "**Team Mystic**\nI am Blanche, leader of Team Mystic. The wisdom of Pokémon is immeasurably deep. I am researching why it is that they evolve.\nMy team? With our calm analysis of overy situation, we cant lose!", {
- file: "https://vignette2.wikia.nocookie.net/pokemon/images/3/35/Blanche-GO.png/revision/latest?cb=20160726200247.png"});
- setTimeout(() => {msg.channel.send("Wat een prachtige woorden vind je niet? Met onze wijsheid kunnen we samen met onze Pokémon elke uitdaging aan!");;}, 1000);
- msg.delete()}
-else if (msgText === "valor") {message.channel.send(
-  "**Team Valor**\nI am Candela, Tem Valor Leader!. Pokemon are stronger than humans, and they are warmhearted, too! I am researching ways to enhance Pokémon natural power in the pursuit of true strength. There is no doubt that the Pokémon in our team have trained are the strongest in battle! Are you ready?", {
-  file: "https://vignette4.wikia.nocookie.net/pokemon/images/c/ce/Candela-GO.png/revision/latest?cb=20160726200643.png"});
-  setTimeout(() => {msg.channel.send("Pokemon sterker dan mensen? Bij jullie team twijfel ik daar soms over.... Dat onderzoek van jullie om pokemon sterker te maken is in elk geval hard nodig bij team faler!");;}, 1000);
-  msg.delete()}
-else if (msgText === "instinct") {message.channel.send(
-  "**Team Instinct**\nHey! The names Spark- the leader of Team Instinct. Pokémon are creatures with excellent intuition. I bet the secret to their intuition is related to how theyre hatched. Come on and join my team! You never lose when you trust your instincts!", {
-  file: "https://vignette2.wikia.nocookie.net/pokemon/images/8/86/Spark-GO.png/revision/latest?cb=20160726200039.png"});
-  setTimeout(() => {msg.channel.send("Vertrouwen op je intuitie? We leven toch zeker niet meer in de steentijd? En wat heeft hebben die eieren daar nu weer mee te maken? Met dit soort uitspraken lijkt het soms alsof je zelf uit een ei bent gekomen...");;}, 1000);
-  msg.delete()}
+   msg.reply("<:game_die:349868481673428992>: " + (Math.floor(Math.random()*6)+1));
+   msg.delete()}
 
- else if ((pokemon = pokemonStats.checkForPokemon(msgText)) != undefined) {
+/* werkt niet :(
+else if (msgText === "mystic") {msg.channel.send(
+  "**Team Mystic**\nI am Blanche, leader of Team Mystic. The wisdom of Pokémon is immeasurably deep. I am researching why it is that they evolve.\nMy team? With our calm analysis of overy situation, we can't lose!",
+   {file: "https://vignette2.wikia.nocookie.net/pokemon/images/3/35/Blanche-GO.png/revision/latest?cb=20160726200247.png" });setTimeout(() =>
+   {msg.channel.send("Wat een prachtige woorden vind je niet? Met onze wijsheid kunnen we samen met onze Pokémon elke uitdaging aan!");;},1000);
+   msg.delete()}
+else if (msgText === "valor") {msg.channel.send(
+  "**Team Valor**\nI am Candela, Team Valor Leader!. Pokemon are stronger than humans, and they are warmhearted, too! I am researching ways to enhance Pokémon natural power in the pursuit of true strength. There is no doubt that the Pokémon in our team have trained are the strongest in battle! Are you ready?",
+   {file: "https://vignette4.wikia.nocookie.net/pokemon/images/c/ce/Candela-GO.png/revision/latest?cb=20160726200643.png" });setTimeout(() =>
+   {msg.channel.send("Pokemon sterker dan mensen? Bij jullie team twijfel ik daar soms over.... Dat onderzoek van jullie om pokemon sterker te maken is in elk geval hard nodig bij team faler!");;},1000);
+   msg.delete()}
+else if (msgText === "instinct") {msg.channel.send(
+  "**Team Instinct**\nHey! The names Spark- the leader of Team Instinct. Pokémon are creatures with excellent intuition. I bet the secret to their intuition is related to how theyre hatched. Come on and join my team! You never lose when you trust your instincts!",
+   {file: "https://vignette2.wikia.nocookie.net/pokemon/images/8/86/Spark-GO.png/revision/latest?cb=20160726200039.png" });setTimeout(() =>
+   {msg.channel.send("Vertrouwen op je intuitie? We leven toch zeker niet meer in de steentijd? En wat hebben die eieren daar nu weer mee te maken? Met dit soort uitspraken lijkt het soms alsof je zelf uit een ei bent gekomen...");;},1000);
+   msg.delete()}
+else if (msgText === "type") {msg.channel.send("Zie hier een tabel met type effectiviteit:",
+   {file: "https://image.ibb.co/mxRthv/Untitled.png"});
+   msg.delete()}
+*/
 
-    pokemonStats.reply(msg, pokemon);
+//pokemon reply
+else if ((pokemon = pokemonStats.checkForPokemon(msgText)) != undefined) {
+    pokemonStats.reply(msg, pokemon);}
 
-  } else if ((gym = Gym.checkForGym(msgText)) != undefined) {
+//gym location reply
+else if ((gym = Gym.checkForGym(msgText)) != undefined) {
+    Gym.reply(msg, gym);}
 
-    Gym.reply(msg, gym);
 
-  } else {
-
-    //message reply
+else {
+//message reply
     const messageMatch = messages.find((message) => {
-      new Message(msg).newMessage(
-        message.keys,
-        message.reply
-      );
+    new Message(msg).newMessage(message.keys, message.reply);
     });
-
 
     if (messageMatch) {
-      msg.reply(`${messageMatch.reply}`);
-    }
+       msg.reply(`${messageMatch.reply}`);}
 
-    //pokestop spins
+//pokestop spins
     const stopMatch = stops.find((stop) => {
-      return msgText.startsWith(stop.key);
-    });
+       return msgText.startsWith(stop.key);});
 
     if (stopMatch) {
-      msg.reply(stopMatch.reply);
-    }
+       msg.reply(stopMatch.reply);}
 
-    //give trusted role, admin only
-    if (msgText.startsWith('add')) {
-      if (msg.member.roles.has(moderator)) {
-        let member = msg.mentions.members.first();
-        let role = msg.guild.roles.find("name",
-          "makingdelftblueagain");
-        member.addRole(role).catch(console.error);
-        msg.channel.send('Welkom ' + member +
-          ', je bent nu officieel toegevoegd! In het kanaal #welkom is te lezen hoe deze discord werkt, lees dat dus vooral eens door! Daarnaast sta ik natuurlijk ook tot je beschikking! Door "!help" te typen kun je zien wat ik allemaal voor je kan doen! Verder zou het fijn zijn als je in deze discord dezelfde naam gebruikt als je pogo naam, zodat we weten wie iedereen is;)'
-        );
-      } else {
-        msg.reply(
-          'Leden verifieren kan alleen door een moderator worden gedaan'
-        )
-      }
-      msg.delete()
-    } else
+//give trusted role, admin only
+  if (msgText.startsWith("add")) {
+    if (msg.member.roles.has(moderator)) {
+       let member = msg.mentions.members.first();
+       let role = msg.guild.roles.find("name",
+       "makingdelftblueagain");
+       member.addRole(role).catch(console.error);
+       msg.channel.send("Welkom " + member +
+       ", je bent nu officieel toegevoegd! In het kanaal #welkom is te lezen hoe deze discord werkt, lees dat dus vooral eens door! Daarnaast sta ik natuurlijk ook tot je beschikking! Door '!help' te typen kun je zien wat ik allemaal voor je kan doen! Verder zou het fijn zijn als je in deze discord dezelfde naam gebruikt als je pogo naam, met je level erachter (channel settings, change nickname), zodat we weten wie iedereen is;)");}
+    else {
+       msg.reply("Leden verifieren kan alleen door een moderator worden gedaan")}
+       msg.delete()}
 
     //request gym roles
-    if (msgText.startsWith('+centrum')) {
+  else if (msgText.startsWith("+centrum")) {
       let role = msg.guild.roles.find("name", "Centrum");
       msg.member.addRole(role).catch(console.error);
-      msg.reply('Je hebt nu toegang tot het centrum gym kanaal!');
-      msg.delete()
-    } else if (msgText.startsWith('+hoven')) {
+      msg.reply("Je hebt nu toegang tot het centrum gym kanaal!");
+      msg.delete()}
+  else if (msgText.startsWith("+hoven")) {
       let role = msg.guild.roles.find("name", "Hoven");
       msg.member.addRole(role).catch(console.error);
-      msg.reply('Je hebt nu toegang tot het hoven gym kanaal!');
-      msg.delete()
-    } else if (msgText.startsWith('+tu')) {
+      msg.reply("Je hebt nu toegang tot het hoven gym kanaal!");
+      msg.delete()}
+  else if (msgText.startsWith("+tu")) {
       let role = msg.guild.roles.find("name", "TU");
       msg.member.addRole(role).catch(console.error);
-      msg.reply('Je hebt nu toegang tot het tu-wijk gym kanaal!');
-      msg.delete()
-    } else if (msgText.startsWith('+tanthof')) {
+      msg.reply("Je hebt nu toegang tot het tu-wijk gym kanaal!");
+      msg.delete()}
+  else if (msgText.startsWith("+tanthof")) {
       let role = msg.guild.roles.find("name", "Tanthof");
       msg.member.addRole(role).catch(console.error);
-      msg.reply('Je hebt nu toegang tot het tanthof gym kanaal!');
-      msg.delete()
-    } else if (msgText.startsWith('+noord')) {
+      msg.reply("Je hebt nu toegang tot het tanthof gym kanaal!");
+      msg.delete()}
+  else if (msgText.startsWith("+noord")) {
       let role = msg.guild.roles.find("name", "DelftNoord");
       msg.member.addRole(role).catch(console.error);
-      msg.reply('Je hebt nu toegang tot het Noord-Delft gym kanaal!');
-      msg.delete()
-    } else if (msgText.startsWith('+oost')) {
+      msg.reply("Je hebt nu toegang tot het Noord-Delft gym kanaal!");
+      msg.delete()}
+  else if (msgText.startsWith("+oost")) {
       let role = msg.guild.roles.find("name", "DelftOost");
       msg.member.addRole(role).catch(console.error);
-      msg.reply('Je hebt nu toegang tot het Oost-Delft gym kanaal!');
-      msg.delete()
-    } else if (msgText.startsWith('+english')) {
+      msg.reply("Je hebt nu toegang tot het Oost-Delft gym kanaal!");
+      msg.delete()}
+  else if (msgText.startsWith("+english")) {
       let role = msg.guild.roles.find("name", "English");
       msg.member.addRole(role).catch(console.error);
-      msg.reply('You now have access to the English channel!');
-      msg.delete()
-    } else
+      msg.reply("You now have access to the English channel!");
+      msg.delete()}
 
     //remove gym roles
-    if (msgText.startsWith('-centrum')) {
+  else if (msgText.startsWith("-centrum")) {
       let role = msg.guild.roles.find("name", "Centrum");
       msg.member.removeRole(role).catch(console.error);
-      msg.reply(
-        'Je hebt nu geen toegang meer tot het centrum gym kanaal!');
-      msg.delete()
-    } else if (msgText.startsWith('-hoven')) {
+      msg.reply("Je hebt nu geen toegang meer tot het centrum gym kanaal!");
+      msg.delete()}
+  else if (msgText.startsWith("-hoven")) {
       let role = msg.guild.roles.find("name", "Hoven");
       msg.member.removeRole(role).catch(console.error);
-      msg.reply(
-        'Je hebt nu geen toegang meer tot het hoven gym kanaal!');
-      msg.delete()
-    } else if (msgText.startsWith('-tu')) {
+      msg.reply("Je hebt nu geen toegang meer tot het hoven gym kanaal!");
+      msg.delete()}
+  else if (msgText.startsWith("-tu")) {
       let role = msg.guild.roles.find("name", "TU");
       msg.member.removeRole(role).catch(console.error);
-      msg.reply(
-        'Je hebt nu geen toegang meer tot het tu-wijk gym kanaal!');
-      msg.delete()
-    } else if (msgText.startsWith('-tanthof')) {
+      msg.reply("Je hebt nu geen toegang meer tot het tu-wijk gym kanaal!");
+      msg.delete()}
+  else if (msgText.startsWith("-tanthof")) {
       let role = msg.guild.roles.find("name", "Tanthof");
       msg.member.removeRole(role).catch(console.error);
-      msg.reply(
-        'Je hebt nu geen toegang meer tot het tanthof gym kanaal!');
-      msg.delete()
-    } else if (msgText.startsWith('-noord')) {
+      msg.reply("Je hebt nu geen toegang meer tot het tanthof gym kanaal!");
+      msg.delete()}
+  else if (msgText.startsWith("-noord")) {
       let role = msg.guild.roles.find("name", "DelftNoord");
       msg.member.removeRole(role).catch(console.error);
-      msg.reply(
-        'Je hebt nu geen toegang meer tot het Noord-Delft gym kanaal!');
-      msg.delete()
-    } else if (msgText.startsWith('-oost')) {
+      msg.reply("Je hebt nu geen toegang meer tot het Noord-Delft gym kanaal!");
+      msg.delete()}
+  else if (msgText.startsWith("-oost")) {
       let role = msg.guild.roles.find("name", "DelftOost");
       msg.member.removeRole(role).catch(console.error);
-      msg.reply(
-        'Je hebt nu geen toegang meer tot het Oost-Delft gym kanaal!');
-      msg.delete()
-    } else if (msgText.startsWith('-english')) {
+      msg.reply("Je hebt nu geen toegang meer tot het Oost-Delft gym kanaal!");
+      msg.delete()}
+  else if (msgText.startsWith("-english")) {
       let role = msg.guild.roles.find("name", "English");
       msg.member.removeRole(role).catch(console.error);
-      msg.reply('You now have lost access to the English channel!');
-      msg.delete()
-    } else
+      msg.reply("You now have lost access to the English channel!");
+      msg.delete()}
 
-    //delete msgs
+//delete msgs
 
-    if (msgText.startsWith('delete')) {
+  else  if (msgText.startsWith("delete")) {
       if (msg.member.roles.has(moderator)) {
-        var del = msgText.split(" ");
-        del.splice(0, 1);
-        msg.channel.bulkDelete(del);
-      } else {
-        msg.reply('Alleen moderators kunnen berichten verwijderen')
-      }
-      msg.delete()
-    }
+         var del = msgText.split(" ");
+         del.splice(0, 1);
+         msg.channel.bulkDelete(del);}
+      else {
+         msg.reply("Alleen moderators kunnen berichten verwijderen")}
+         msg.delete()}
+
   }
 });
 
 
 //welcome new users
-
-client.on('guildMemberAdd', member => {
+client.on("guildMemberAdd", member => {
   setTimeout(() => {
     member.guild.defaultChannel.send(
-      `Welkom op de Mystic Delft Discord en leuk dat je onze groep wilt versterken, ${member}!
-Om toegang te krijgen tot de volledige groep vragen wij een screenshot van je pokemon go profiel (dat is waar je naast je buddy staat). Als je deze kan uploaden zal een van de moderators je zo snel mogelijk te woord staan.
-
-Welcome to our Mystic Delft Discord group, ${member}!
-In order to get full access to our server, we would like to verify you are indeed mystic. If  you would be so kind as to upload a screenshot of your Pokémon Go profile (where you are standing next to your buddy) one of our moderators will contact you as soon as possible.`
-    );
-  }, 1000);
+    "Welkom op de Mystic Delft Discord en leuk dat je onze groep wilt versterken, ${member}!\nOm toegang te krijgen tot de volledige groep vragen wij een screenshot van je pokemon go profiel (dat is waar je naast je buddy staat). Als je deze kan uploaden zal een van de moderators je zo snel mogelijk te woord staan.\n\nWelcome to our Mystic Delft Discord group, ${member}!\nIn order to get full access to our server, we would like to verify you are indeed mystic. If  you would be so kind as to upload a screenshot of your Pokémon Go profile (where you are standing next to your buddy) one of our moderators will contact you as soon as possible.");
+    }, 1000);
 });
