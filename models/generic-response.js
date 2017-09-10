c345c3b1e642fbcb064dab3635d520352a0f978e
Glenn Vlekke <g.vlekke@sping.nl>
2017-09-10 21:37:07 +0200

    rename class

--- models/generic-response.js
+++ models/generic-response.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const Message = require('./message');
 const replies = require('../data/reply.json');
 
-module.exports = class GymResponse extends Message {
+module.exports = class GenericResponse extends Message {
   constructor(message) {
     super(message);
     this.replyObject = this.getReply();
