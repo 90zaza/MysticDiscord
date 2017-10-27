8a220956e92eeeda2e42ca194591d868b5525723
petiebarie <30798422+petiebarie@users.noreply.github.com>
2017-10-27 15:23:17 +0200

    rewrite adding roles system using !+ and !- command

--- models/channel-roles-response.js
+++ models/channel-roles-response.js
@@ -1,12 +1,17 @@
 const Discord = require('discord.js');
-const Message = require('./message');
+const Message = require('./message.js');
 const channelRoles = require('../data/channel-roles.json');
 
 module.exports = class GenericResponse extends Message {
   constructor(message) {
     super(message);
+    var action
+    if (msg.content.startsWith("+")) {action = "add"}
+    if (msg.content.startsWith("-")) {action = "remove"}
+    message = message.substr(1);
+    console.log(message);
     this.channelRole = this.getChannelRole();
-
+    console.log('test');
     if(this.channelRole) {
       this.executeRoleChange();
     }
@@ -17,7 +22,6 @@ module.exports = class GenericResponse extends Message {
   }
 
   async executeRoleChange() {
-    const action = this.channelRole.action;
     try {
       const role = this.message.guild.roles.find("name", this.channelRole.name);
       if(action === 'add') {
@@ -26,9 +30,17 @@ module.exports = class GenericResponse extends Message {
         console.log('removing role');
         await this.message.member.removeRole(role)
       }
-      this.reply(this.channelRole.reply)
+      if (this.channelRole.sort == region) {
+        this.reply(`Je hebt nu toegang tot het ${this.channelRole.name} kanaal`)
+      }
+      if (this.channelRole.sort == english) {
+        this.reply(`You now have access to the English channel`)
+      }
+      if (this.channelRole.sort == pokemon) {
+        this.reply(`Je ontvangt nu extra notificaties voor ${this.channelRole.name} raids`)
+      }
     } catch (error) {
-      this.reply('Oepsie, misschien ergens een typ fout?')
+      this.reply('Rol niet gevonden, typfoutje?')
       console.error(error);
     }
   }
