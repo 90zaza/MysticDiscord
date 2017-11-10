d4fa42e8aed7e295320c4769a34129b4ef4cd590
petiebarie <30798422+petiebarie@users.noreply.github.com>
2017-11-10 01:27:00 +0100

    finishing touches

--- models/whosthatpokemon-response.js
+++ models/whosthatpokemon-response.js
@@ -11,36 +11,32 @@ module.exports = class GenericResponse extends Message {
     let rand;
 
     //gen 1
-    if(this.startsWith('whosthatpokemon gen 1') || this.startsWith('wieisdezepokemon gen 1')) {
+    if(this.startsWith('whosthatpokemon gen 1') || this.startsWith('wieisdezepokemon gen 1') || this.startsWith('whosthatpokemon gen1') || this.startsWith('wieisdezepokemon gen1')) {
       rand = Math.floor(Math.random() * 151);
-      console.log("\n1\n");
     }
    //gen 2
-    else if(this.startsWith('whosthatpokemon gen 2') || this.startsWith('wieisdezepokemon gen 2')) {
+    else if(this.startsWith('whosthatpokemon gen 2') || this.startsWith('wieisdezepokemon gen 2') || this.startsWith('whosthatpokemon gen2') || this.startsWith('wieisdezepokemon gen2')) {
      rand = Math.floor(Math.random() * 251);
-     console.log("\n2\n");
    }
    //gen 3
-    else if(this.startsWith('whosthatpokemon gen 3') || this.startsWith('wieisdezepokemon gen 3')) {
+    else if(this.startsWith('whosthatpokemon gen 3') || this.startsWith('wieisdezepokemon gen 3') || this.startsWith('whosthatpokemon gen3') || this.startsWith('wieisdezepokemon gen3')) {
      rand = Math.floor(Math.random() * 386);
-     console.log("\n3\n");
    }
    //gen 4
-    else if(this.startsWith('whosthatpokemon gen 4') || this.startsWith('wieisdezepokemon gen 4')) {
+    else if(this.startsWith('whosthatpokemon gen 4') || this.startsWith('wieisdezepokemon gen 4') || this.startsWith('whosthatpokemon gen4') || this.startsWith('wieisdezepokemon gen4')) {
      rand = Math.floor(Math.random() * 493);
    }
    //gen 5
-    else if(this.startsWith('whosthatpokemon gen 5') || this.startsWith('wieisdezepokemon gen 5')) {
+    else if(this.startsWith('whosthatpokemon gen 5') || this.startsWith('wieisdezepokemon gen 5') || this.startsWith('whosthatpokemon gen5') || this.startsWith('wieisdezepokemon gen5')) {
      rand = Math.floor(Math.random() * 649);
    }
    //gen 6
-    else if(this.startsWith('whosthatpokemon gen 6') || this.startsWith('wieisdezepokemon gen 6')) {
+    else if(this.startsWith('whosthatpokemon gen 6') || this.startsWith('wieisdezepokemon gen 6') || this.startsWith('whosthatpokemon gen6') || this.startsWith('wieisdezepokemon gen6')) {
      rand = Math.floor(Math.random() * 721);
    }
    //gen 7
    else{
      rand = Math.floor(Math.random() * 802);
-     console.log("\n7\n");
    }
 
    if (rand<10){
@@ -49,7 +45,6 @@ module.exports = class GenericResponse extends Message {
    else if (rand<100){
      rand = "0"+rand;
    }
-
 
    const img = `https://i0.wp.com/delftmystic.files.wordpress.com/2017/11/silhouette-${rand}1.png`
    const embed = new Discord.MessageEmbed()
